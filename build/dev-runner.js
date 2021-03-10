26cac4539f10886ac3615d76bd028b1d57d1d5e1
czh6865801 <2251668603@qq.com>
2021-03-10 23:44:42 +0800

    修改

--- build/dev-runner.js
+++ build/dev-runner.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const { exec } = require('child_process')
+const platform = require('process').platform
 
-// kill -9 
-exec('pgrep node.exe | xargs ps -u --pid',(error, stdout, stderr)=>{
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-})
+//启动前杀死所有node进程。防止端口占用
+if (platform !== 'win32') {
+  exec('kill -9 `lsof -t -i: ' + port +'`',(error)=>{
+    console.error(error)
+  })
+} else {
+  exec('tskill node',(error)=>{
+    console.error(error)
+  })  
+}
+
 
 
 
