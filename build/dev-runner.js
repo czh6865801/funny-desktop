5e80aa7bc3a04ff3db426684664c6dcedf4c26de
czh6865801 <2251668603@qq.com>
2021-03-13 17:53:33 +0800

    修改

--- build/dev-runner.js
+++ build/dev-runner.js
@@ -5,14 +5,24 @@ const platform = require('process').platform
 
 //启动前杀死所有node进程。防止端口占用
 if (platform !== 'win32') {
-  exec('kill -9 `lsof -t -i: ' + port +'`',(error)=>{
+  exec('kill -9 lsof -t -i: 9080', (error) => {
     console.error(error)
   })
 } else {
-  exec('tskill node',(error)=>{
-    console.error(error)
-  })  
+  var a = exec('netstat -ano | findstr 9080')
+  a.stdout.on('data', (stdout) => {
+    let b = stdout.split('  ').pop().split('')
+    let c = []
+    for (let index = 0; index < b.length; index++) {
+      if (/^\d+$/.test(b[index])) {
+        c.push(b[index])
+      }
+    }
+    exec(`taskkill /pid ${c.join('')} -t -f`)
+  })
 }
+
+
 
 
 
