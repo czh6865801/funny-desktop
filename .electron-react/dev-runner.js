3123df1ea60bcc0657bc91a0125ccf7418965958
czh6865801 <2251668603@qq.com>
2021-03-31 22:05:30 +0800

    xiugai

--- .electron-react/dev-runner.js
+++ .electron-react/dev-runner.js
@@ -40,4 +40,4 @@ if (platform !== 'win32') {
 }
 
 console.log(`${YELLOW}Starting webpack serve...\n${END}`)
-run('webpack serve --config ./webpack/webpack.dev.js', YELLOW, 'webpack')
+run('webpack serve --config .electron-react/webpack.dev.js', YELLOW, 'webpack')
