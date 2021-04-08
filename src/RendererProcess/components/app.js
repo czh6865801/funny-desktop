86f99fec36d8b287d52cf9b99d61838a1dc80782
chenzh66 <chenzh66@midea.com>
2021-04-08 23:04:04 +0800

    xiugai

--- src/RendererProcess/components/app.js
+++ src/RendererProcess/components/app.js
@@ -1,11 +1,14 @@
 import React from "react"
+import Search from './search'
 import './app.less'
 export default class Login extends React.Component {
   render() {
     return (
       <>
-        <div>左</div>
-        <div>右</div>
+        <div className="layout_left">
+          <Search></Search>
+        </div>
+        <div className="layout_right">右</div>
       </>
     )
   }
