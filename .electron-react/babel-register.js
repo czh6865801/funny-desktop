c0fbb0b1e4e32356ae21727e10621dd4cf99c90f
chenzh66 <chenzh66@midea.com>
2021-05-13 15:45:36 +0800

    修改引用

--- .electron-react/babel-register.js
+++ .electron-react/babel-register.js
@@ -2,5 +2,6 @@ const path = require('path');
 
 require('@babel/register')({
   extensions: ['.es6', '.es', '.jsx', '.js', '.mjs', '.ts', '.tsx'],
+  ignore: [/node_modules/],
   cwd: path.join(__dirname, '../'),
 });
